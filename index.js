c91887167b9a0c0caf0c023cac55785e3a0e92a3
Vinh Nguyen <npcv4129@gmail.com>
2022-02-14 23:22:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>res.send("Hello word"))
+
+app.listen(port,()=>{
+    console.log(`App listening on http://localhost:${port}`);
+})
